367887ecabd2aef3f2ff7dad73fd182f08a4737c
jianghuawang <jianghuawang@ucsb.edu>
2020-04-20 13:27:52 -0700

    add js function

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
-function activateTab(tabId){
-    var mytab=document.querySelector(tabId);
-    mytab.style.backgroundColor="#90ee90";
-    mytab.style.color="#FFFFFF"; 
+
+window.addEventListener("scroll",function(){
+    var bttn=document.getElementById("back");
+    if(document.body.scrollTop>20|| document.documentElement.scrollTop>20){
+        bttn.style.visibility="visible";
+    }
+    else{
+        bttn.style.visibility="hidden";
+    }
+})
+function backtoTop(){
+    document.body.scrollTop=0;
+    document.documentElement.scrollTop=0;
+}
+function enlarge(mysrc){
+    var modal=document.getElementById("myModel");
+    var modalImg = document.getElementById("img01");
+    modal.style.display="block";
+    modalImg.src=mysrc;
+
 }
